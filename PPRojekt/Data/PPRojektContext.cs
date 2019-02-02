644728d4dffb905d6f7af23820e38295a744f5ad
Kornel <kornelbudz1998@o2.pl>
2019-02-02 22:49:33 +0100

    First commit

---
+++ PPRojekt/Data/PPRojektContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PPRojekt.Models
+{
+    public class PPRojektContext : DbContext
+    {
+        public PPRojektContext (DbContextOptions<PPRojektContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PPRojekt.Models.User> User { get; set; }
+    }
+}
